0c29af6609ade7945c290988a568a82b878715af
narduc <radnielpadron@gmail.com>
2020-08-11 11:52:36 -0700

    pushing code

--- src/components/CourseContainer.js
+++ src/components/CourseContainer.js
@@ -3,13 +3,54 @@ import CourseDetails from "./CourseDetails";
 import CourseSelector from "./CourseSelector";
 import StudentsList from "./StudentsList";
 
+
+const API_STUDENTS = "http://localhost:6001/students";
+
 class CourseContainer extends Component {
+
+    //     "id": 272,
+    //     "name": "Having a good time",
+    //     "instructor": "Rob Boss",
+    //     "semester": "Summer 2020"
+    //   }
+    // "students": [
+    //     "id": 5945,
+    //     "name": "Mr. Princess Predovic",
+    //     "class_year": 11,
+    //     "percentage": 93,
+    //     "course": "Having a good time",
+    //     "attending": true
+
+
+    constructor() {
+        super();
+        this.state = {
+          students: []
+        };
+      }
+
+      componentDidMount() {
+        fetch(API_STUDENTS)
+          .then((res) => res.json())
+          .then((obj) => {
+            return this.setState({
+              students: obj,
+            });
+          });
+      }
+
+
+
   render() {
+
+    //   console.log(this.props.courses);
+    //   console.log(this.state.students);
+
     return (
       <div className="ui grid container">
         <CourseDetails />
         <CourseSelector />
-        <StudentsList />
+        <StudentsList students={this.state.students}/>
       </div>
     );
   }
--- src/components/StudentsList.js
+++ src/components/StudentsList.js
@@ -1,12 +1,25 @@
 import React from "react";
 import Student from "./Student";
 
-const StudentsList = () => (
+// displayStudents = (props.students) => {
+//     const students_obj = ({this.props.students})
+//     return students_obj;
+// }
+// console.log(displayStudents(props.students))
+
+const StudentsList = (CourseContainer) => (
+    // console.log(displayStudents())
+    // console.log(props.students);
+    // console.log(CourseContainer);
+
+    // how to receive props from a class to an arrow function?
+    
   <table className="ui celled striped padded table unstackable">
     <tbody>
       <tr>
         <th>
           <h3 className="ui center aligned header">Student Name</h3>
+          {/* {props.students} */}
         </th>
         <th>
           <h3 className="ui center aligned header">Class Year</h3>
